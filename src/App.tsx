a51d098da7f08fbeb3fdeeeecee8cd9df4c18b83
minhnghialk <nghia13031995@gmail.com>
2023-10-13 17:42:12 +0700

    edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import React from 'react'
+import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Map from './pages/Map';
-import Login from './pages/Login';
-import NavBar from './pages/NavBar';
-import Admin from './pages/admin/Admin';
-
-
+import Map from "./pages/Map";
+import Login from "./pages/Login";
+import NavBar from "./pages/NavBar";
+import Admin from "./pages/admin/Admin";
 
 export default function App() {
   return (
     <div>
       <NavBar></NavBar>
-
+      <h1>abc</h1>
 
       <Routes>
-      <Route path="/admin" element={<Admin />} />
-      <Route path="/" element={<Map />} />
-      <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/" element={<Map />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
-  )
+  );
 }
